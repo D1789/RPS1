130c1d93d87d2078793c796506699bf8ffa6df36
Dan Belfield <dan.belfield@live.co.uk>
2018-02-06 20:48:15 +0000

    script and html corections. Now working. Started styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,39 +1,38 @@
 //declarations
-let options = ["rock", "paper", "scissors"];
+let options = ["ROCK", "PAPER", "SCISSORS"];
 let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissors = document.querySelector("#scissors");
 
+//users choice event listeners
+rock.addEventListener('click', round);
+paper.addEventListener('click', round);
+scissors.addEventListener('click', round);
+
 //computers choice
 function computerPlay() {
   let computerSelection = options[Math.floor(Math.random()*options.length)];
-  return computerSelection.toLowerCase();
+  return computerSelection.toUpperCase();
 }
-
-//users choice event listeners
-rock.addEventListener('click',round);
-paper.addEventListener('click',round);
-scissors.addEventListener('click',round);
 
 //
 let outcome = document.querySelector(".outcome");
 let result = document.createElement("div");
 outcome.append(result);
 
-
 //one round
 function round() {
   let computerChoice = computerPlay().toUpperCase();
   let playerChoice = this.id.toUpperCase();
   if (computerChoice === playerChoice) {
-    result.textContent = "Draw! Both players selected ${playerChoice}"
+    result.textContent = "Draw! Both players selected " + playerChoice +"!";
 } else if (
   (playerChoice === "ROCK" && computerChoice === "SCISSORS") ||
   (playerChoice === "SCISSORS" && computerChoice === "PAPER") ||
   (playerChoice === "PAPER" && computerChoice === "ROCK")
 ) {
-  result.textContent = "You WIN this round! Have a point!"
+  result.textContent = "You WIN this round! Have a point!";
 } else {
-  result.textContent = "Sorry, you LOSE this round. Computer gets a point..."
+  result.textContent = "Sorry, you LOSE this round. Computer gets a point...";
 }
 }
